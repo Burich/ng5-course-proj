bd8a70f7e5173d2c4ac6e870076823f3fc8b6919
Burich <BurichCaeser@gmail.com>
2018-02-18 20:23:41 +0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/mail/inbox', pathMatch: 'full'},
+  {path: 'signin', component: NotFoundComponent},
+  {path: 'mail', redirectTo: '/mail/inbox', pathMatch: 'full'},
+  {path: 'mail/inbox', component: NotFoundComponent},
+  {path: 'mail/outox', component: NotFoundComponent},
+  {path: 'mail/new', component: NotFoundComponent},
+  {path: 'mail/letter/:id', component: NotFoundComponent},
+  {path: 'mail/users', component: NotFoundComponent},
+  {path: 'mail/users/new', component: NotFoundComponent},
+  {path: 'mail/users/:id', component: NotFoundComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
