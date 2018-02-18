aa8185718e4a4a4d8e502c001504e82f4676fc53
Burich <BurichCaeser@gmail.com>
2018-02-18 23:28:20 +0300

    Заготовки форм

--- src/app/addrs/addrs.component.ts
+++ src/app/addrs/addrs.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addrs.component.css']
 })
 export class AddrsComponent implements OnInit {
+  // TODO: определять тип входящие/исходящие и менять содержимое в зависимости от этого
+  // TODO: модели, погрузка данных по страницам
+  users = [
+    {id: 1, author: 'saved name', email: 'his@gmail.com'},
+    {id: 2, author: '', email: 'no-name@gmail.com'}
+  ];
 
   constructor() { }
 
--- src/app/mailbox/mailbox.component.ts
+++ src/app/mailbox/mailbox.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MailboxComponent implements OnInit {
 
+  // TODO: определять тип входящие/исходящие и менять содержимое в зависимости от этого
+  // TODO: модели, погрузка данных по страницам
+  letters = [
+    {id: 1, author: 'saved name', title: 'some title'},
+    {id: 2, author: 'no-name@gmail.com', title: 'another title'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
